80330724e7597e5bcf75cb43709379c9bff1429f
Ryan Giordano <rgiordan@gmail.com>
2015-07-16 08:30:11 -0700

    Covariances

--- src/JuMPVariationalBayes.jl
+++ src/JuMPVariationalBayes.jl
@@ -1,10 +1,13 @@
+# To use, you must clone directly from github:
+# https://github.com/rgiordan/JuMPVariationalBayes.jl
+
 module JuMPVariationalBayes
 
-#require(joinpath(Pkg.dir("JuMPVariationalBayes"), "src", "Covariances.jl"))
+require(joinpath(Pkg.dir("JuMPVariationalBayes"), "src", "Covariances.jl"))
 require(joinpath(Pkg.dir("JuMPVariationalBayes"), "src", "SubModels.jl"))
 require(joinpath(Pkg.dir("JuMPVariationalBayes"), "src", "HessianReparameterization.jl"))
 
-#import Covariances
+import Covariances
 import SubModels
 import HessianReparameterization
 

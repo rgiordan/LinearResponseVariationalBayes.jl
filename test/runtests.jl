84bcf7cf794b6275e3e3fedd185b4f2c83ed4d21
Ryan Giordano <rgiordan@gmail.com>
2015-09-07 15:23:10 -0700

    fix runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,3 +1,5 @@
+using LinearResponseVariationalBayes
+
 include(joinpath(Pkg.dir("LinearResponseVariationalBayes"), "test", "test_hessian_reparam.jl"))
 include(joinpath(Pkg.dir("LinearResponseVariationalBayes"), "test", "test_exponential_families.jl"))
 include(joinpath(Pkg.dir("LinearResponseVariationalBayes"), "test", "test_sub_models.jl"))
